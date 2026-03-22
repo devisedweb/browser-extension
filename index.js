5ace7d931d9cf42bd4f2e8ad7cbb9e3979a75af0
DevisedWeb <contact@devisedweb.com>
2026-03-22 21:03:10 +0500

    Started working on JS

---
+++ index.js
@@ -0,0 +1,5 @@
+const inputEl = document.getElementById("input-el");
+const saveInput = document.getElementById("save-in");
+const saveBtn = document.getElementById("save-btn");
+const ulEl = document.getElementById("ul-el");
+const delBtn = document.getElementById("del-btn");
