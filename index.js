a6e970e5abf13fa31731447da63831edda481bc3
DevisedWeb <contact@devisedweb.com>
2026-03-24 14:26:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const saveInput = document.getElementById("save-in");
 const saveBtn = document.getElementById("save-btn");
 const ulEl = document.getElementById("ul-el");
 const delBtn = document.getElementById("del-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+saveInput.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+const tabs = [{ url: "https://www.google.com" }];
+
+saveBtn.addEventListener("click", function () {
+  myLeads.push(tabs[0].url);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+function render(leads) {
+  let listItems = "";
+
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+              <li>
+                <a href="${leads[i]}" target="_blank">
+                  ${leads[i]}
+                </a>
+              </li>
+          `;
+  }
+
+  ulEl.innerHTML = listItems;
+}
+
+delBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
